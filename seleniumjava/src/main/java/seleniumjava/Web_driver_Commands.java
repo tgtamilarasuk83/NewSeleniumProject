f8bf8388bb159de33e1e49e4ceb4587f875045ed
Tamilarasu143 <tamilarasu143@gmail.com>
2026-04-01 14:02:04 +0530

    commited

--- seleniumjava/src/main/java/seleniumjava/Web_driver_Commands.java
+++ seleniumjava/src/main/java/seleniumjava/Web_driver_Commands.java
@@ -4,12 +4,13 @@ package seleniumjava;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Web_driver_Commands {
 
 	public static void main(String[] args) {
 
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 
 		driver.get("https://www.flipkart.com/");
 
